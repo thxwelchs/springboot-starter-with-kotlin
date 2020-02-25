6738d37481da4590a34eeccb92fe271b09f31b01
thxwelchs <thxwelchs@gmail.com>
2020-02-25 12:56:58 +0900

    MenuRepositoryTest

--- src/test/kotlin/com/example/demo/repository/MenuRepositoryTest.kt
+++ src/test/kotlin/com/example/demo/repository/MenuRepositoryTest.kt
@@ -1,29 +1,25 @@
 package com.example.demo.repository
 
+import org.junit.jupiter.api.Assertions.assertTrue
 import org.junit.jupiter.api.Test
-
-import org.junit.jupiter.api.Assertions.*
-import org.junit.jupiter.api.extension.ExtendWith
 import org.springframework.beans.factory.annotation.Autowired
+import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest
-import org.springframework.boot.test.context.SpringBootTest
 import org.springframework.test.context.ActiveProfiles
 import org.springframework.test.context.TestConstructor
-import org.springframework.test.context.junit.jupiter.SpringExtension
-import org.springframework.test.context.web.WebAppConfiguration
 
 @TestConstructor(autowireMode = TestConstructor.AutowireMode.ALL)
-@ActiveProfiles("local")
-//@ExtendWith(SpringExtension::class)
-@WebAppConfiguration
+@ActiveProfiles("test")
+@AutoConfigureTestDatabase(replace = AutoConfigureTestDatabase.Replace.NONE)
 @DataJpaTest
-internal class MenuRepository @Autowired constructor(
+internal class MenuRepositoryTest @Autowired constructor(
         private val menuRepository: MenuRepository
 ) {
 
     @Test
-    fun findAllJoinRestaurant() {
-        menuRepository.findAllJoinRestaurant()
-        println(" = ")
+    fun `Menu 만 조회했는데, Restaurant 가 조회되는 N+1 현상 테스트` () {
+        val menus = menuRepository.findAllJoinRestaurant()
+
+        assertTrue(true)
     }
 }
