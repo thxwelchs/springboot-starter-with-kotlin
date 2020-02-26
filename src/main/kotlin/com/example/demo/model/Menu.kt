31aa469267f62ac2d65058ccabab986dee95c99e
thxwelchs <thxwelchs@gmail.com>
2020-02-26 17:13:19 +0900

    양방향 설정

--- src/main/kotlin/com/example/demo/model/Menu.kt
+++ src/main/kotlin/com/example/demo/model/Menu.kt
@@ -1,14 +1,18 @@
 package com.example.demo.model
 
 import com.example.demo.model.entity.MutableEntity
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties
 import java.math.BigDecimal
 import javax.persistence.Entity
 import javax.persistence.FetchType
+import javax.persistence.JoinColumn
 import javax.persistence.ManyToOne
 
 @Entity
-data class Menu(
+@JsonIgnoreProperties(value=["hibernateLazyInitializer", "handler"])
+class Menu(
         @ManyToOne(fetch = FetchType.LAZY)
+        @JoinColumn(name = "restaurant_id")
         var restaurant: Restaurant,
         var price: BigDecimal = BigDecimal.ZERO,
         var name: String = ""
--- src/main/kotlin/com/example/demo/model/Restaurant.kt
+++ src/main/kotlin/com/example/demo/model/Restaurant.kt
@@ -1,9 +1,24 @@
 package com.example.demo.model
 
 import com.example.demo.model.entity.MutableEntity
+import com.fasterxml.jackson.annotation.JsonIdentityInfo
+import com.fasterxml.jackson.annotation.ObjectIdGenerators.PropertyGenerator
 import javax.persistence.Entity
+import javax.persistence.FetchType
+import javax.persistence.OneToMany
+import javax.persistence.OrderBy
 
 @Entity
-data class Restaurant(
-        var name: String = ""
-): MutableEntity()
+@JsonIdentityInfo(generator = PropertyGenerator::class, property = "id")
+class Restaurant(
+        var name: String = "",
+        @OneToMany(mappedBy = "restaurant", fetch = FetchType.LAZY)
+        @OrderBy("name asc")
+        var menus: MutableList<Menu> = ArrayList()
+): MutableEntity() {
+
+        fun addMenu(menu: Menu) {
+                menus.add(menu)
+                menu.restaurant = this
+        }
+}
